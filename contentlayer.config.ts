ebb8d33ed45eb6c9738273683f2983051d33c809
Chin <greeting.to.chin@gmail.com>
2025-01-29 16:59:20 +0800

    feat: add reading time

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,6 +2,7 @@ import rehypeCodeTitles from 'rehype-code-titles';
 import rehypePrism from 'rehype-prism-plus';
 import rehypeSlug from 'rehype-slug';
 import { defineDocumentType, makeSource } from 'contentlayer2/source-files';
+import readingTime from 'reading-time';
 import imageMetadata from './script/image-metadata';
 
 export const Post = defineDocumentType(() => ({
@@ -17,6 +18,7 @@ export const Post = defineDocumentType(() => ({
   },
   computedFields: {
     path: { type: 'string', resolve: (post) => `/posts/${post.slug}` },
+    reading: { type: 'number', resolve: (post) => Math.ceil(readingTime(post.body.raw).minutes) },
   },
 }));
 
--- src/components/Post/List/index.tsx
+++ src/components/Post/List/index.tsx
@@ -1,45 +1,43 @@
 import Balancer from 'react-wrap-balancer';
 import Link from '~/components/Link';
 import formatDate from '~/helpers/format-date';
+import { Post } from '~/helpers/get-posts';
 
-export interface PostMeta {
-  slug: string;
-  date: string;
-  title: string;
-  description: string;
-  path: string;
-}
+const T = {
+  readingUnit: 'min',
+};
 
 type Props = {
-  posts: PostMeta[];
+  posts: Post[];
 };
 
 export default function PostList({ posts }: Props) {
   return (
     <ul className="transition-colors divide-y divide-gray-200 dark:divide-gray-700">
-      {posts.map((post) => {
-        const { slug, date, title, description, path } = post;
+      {posts.map(({ slug, date, title, description, path, reading }) => {
         return (
           <li key={slug} className="group list-none transition-colors">
             <Link href={path} className="decoration-none">
-              <article className="rounded-xl p-4 transition-colors xl:grid xl:grid-cols-4 xl:items-baseline space-y-2 group-hover:bg-gray-100 xl:space-y-0 dark:group-hover:bg-gray-800">
-                <dl>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-sm text-gray-500 font-medium leading-6 transition-colors md:text-base dark:text-gray-400">
-                    <time dateTime={date}>{formatDate(date)}</time>
-                  </dd>
-                </dl>
-                <main className="xl:col-span-3 space-y-3">
-                  <h3 className="text-lg text-gray-800 font-bold tracking-tight transition-colors md:text-2xl sm:text-xl dark:text-gray-100">
-                    {title}
-                  </h3>
-                  <Balancer
-                    as="p"
-                    className="max-w-none text-gray-500 transition-colors dark:text-gray-400 md:leading-loose"
-                  >
-                    {description}
-                  </Balancer>
-                </main>
+              <article className="rounded-xl p-4 transition-colors space-y-2 group-hover:bg-gray-100 xl:space-y-0 dark:group-hover:bg-gray-800">
+                <header className="flex flex-col md:flex-row md:items-center md:gap-3">
+                  <h3 className="text-lg font-bold tracking-tight transition-colors">{title}</h3>
+                  <span className="flex gap-1 text-sm tracking-wide">
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd>
+                        <time dateTime={date}>{formatDate(date)}</time>
+                      </dd>
+                    </dl>
+                    · {reading}
+                    {T.readingUnit}
+                  </span>
+                </header>
+                <Balancer
+                  as="p"
+                  className="max-w-none text-gray-500 transition-colors md:mt-1 dark:text-gray-400 md:leading-loose"
+                >
+                  {description}
+                </Balancer>
               </article>
             </Link>
           </li>
