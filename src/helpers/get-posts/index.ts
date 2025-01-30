f0c0e9cbb53e75c72a0462ad72327dfefe7f869c
Chin <greeting.to.chin@gmail.com>
2025-01-30 21:07:03 +0800

    fix: descending order

--- src/helpers/get-posts/index.ts
+++ src/helpers/get-posts/index.ts
@@ -1,7 +1,9 @@
 import { allPosts } from 'contentlayer/generated';
 export type { Post } from 'contentlayer/generated';
 
-const POST_LIST = allPosts.sort((a, b) => Number(a) - Number(b));
+const getNumericDate = (date: string) => Number(date.substring(0, 10).replace(/-/g, ''));
+
+const POST_LIST = allPosts.sort((a, b) => (getNumericDate(a.date) > getNumericDate(b.date) ? -1 : 1));
 
 export const getPosts = () => POST_LIST;
 export const getPostBySlug = (slug: string) => POST_LIST.find((post) => post.slug === slug);
